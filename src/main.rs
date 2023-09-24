f8f04a31ac1206cd1350b95f8418bda4b4d23253
AbianS <abiansuarezbrito@gmail.com>
2023-09-24 21:20:37 +0100

    style: :art: format code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+use colored::*;
+use loading::Loading;
 use piechart::{Chart, Color, Data};
 use std::collections::HashMap;
 use std::process::{Command, Stdio};
-use std::{str, thread, env};
-use loading::Loading;
-use colored::*;
+use std::{env, str, thread};
 
 /// Executes a command in PowerShell and returns the output as a string.
 ///
@@ -20,10 +20,12 @@ fn run_command(command: &str) -> String {
         .output()
         .expect("Failed to execute command");
 
-    str::from_utf8(&output.stdout).expect("Invalid UTF-8").to_string()
+    str::from_utf8(&output.stdout)
+        .expect("Invalid UTF-8")
+        .to_string()
 }
 
-fn get_header(){
+fn get_header() {
     let text = r#"
    ___ _ _       ___          _        __ _        _       
   / _ (_) |_    / __\___   __| | ___  / _\ |_ __ _| |_ ___ 
@@ -36,13 +38,9 @@ fn get_header(){
     println!("{}", text);
 }
 
-
 /// Clears the terminal screen.
 fn clear_terminal() {
-    let _ = Command::new("cmd")
-        .arg("/c")
-        .arg("cls")
-        .status();
+    let _ = Command::new("cmd").arg("/c").arg("cls").status();
 }
 
 /// Returns a vector of unique authors in the Git repository.
@@ -57,21 +55,23 @@ fn clear_terminal() {
 fn get_unique_authors() -> Vec<String> {
     let command: &str = "git log --format='%aN' | Sort-Object -Unique";
     let stdout: String = run_command(command);
-    let authors: Vec<String> = stdout.trim().split('\n').map(|author| author.trim().to_string()).collect();
+    let authors: Vec<String> = stdout
+        .trim()
+        .split('\n')
+        .map(|author| author.trim().to_string())
+        .collect();
     authors
 }
 
-
-
 /// This function takes an author name as input and returns a tuple containing the author name, the number of lines inserted by the author, and the number of files changed by the author.
 /// It uses the `git log` command to get the commit history of the repository and extracts the number of files changed, lines inserted and lines deleted by the given author using regular expressions.
-/// 
+///
 /// # Arguments
-/// 
+///
 /// * `author` - A string slice that holds the name of the author whose statistics are to be calculated.
-/// 
+///
 /// # Returns
-/// 
+///
 /// A tuple containing the author name, the number of lines inserted by the author, and the number of files changed by the author.
 fn get_git_stats(author: &str) -> (String, i32, i32) {
     let command: String = format!("git log --shortstat --author=\"{}\"", author);
@@ -81,7 +81,9 @@ fn get_git_stats(author: &str) -> (String, i32, i32) {
     let mut lines_inserted: i32 = 0;
     let mut lines_deleted: i32 = 0;
 
-    let re: regex::Regex = regex::Regex::new(r"(\d+) files? changed, (\d+) insertions\(\+\), (\d+) deletions\(-\)").unwrap();
+    let re: regex::Regex =
+        regex::Regex::new(r"(\d+) files? changed, (\d+) insertions\(\+\), (\d+) deletions\(-\)")
+            .unwrap();
 
     for line in stdout.lines() {
         if let Some(captures) = re.captures(line) {
@@ -96,12 +98,12 @@ fn get_git_stats(author: &str) -> (String, i32, i32) {
 
 fn main() {
     // Gets the current directory and sets it as the working directory.
-    let current_dir: std::path::PathBuf = env::current_dir().expect("Failed to get current directory");
+    let current_dir: std::path::PathBuf =
+        env::current_dir().expect("Failed to get current directory");
     env::set_current_dir(current_dir).expect("Failed to set current directory");
 
     clear_terminal();
     get_header();
-
 
     // Checks if the current directory is a git repository by running the `git rev-parse --is-inside-work-tree` command.
     // Returns a boolean indicating whether the current directory is a git repository or not.
@@ -122,7 +124,6 @@ fn main() {
         return; // Salir del programa
     }
 
-
     let loading: Loading = Loading::default();
     loading.text("Getting git stats...".blue());
 
@@ -130,11 +131,11 @@ fn main() {
     let mut author_stats: HashMap<String, (f32, Color)> = HashMap::new();
 
     let colors = vec![
-        Color::RGB(255, 0, 0), 
-        Color::RGB(0, 255, 0),  
-        Color::RGB(0, 0, 255),  
-        Color::RGB(255, 255, 0),  
-        Color::RGB(255, 0, 255), 
+        Color::RGB(255, 0, 0),
+        Color::RGB(0, 255, 0),
+        Color::RGB(0, 0, 255),
+        Color::RGB(255, 255, 0),
+        Color::RGB(255, 0, 255),
         Color::RGB(0, 255, 255),
         Color::RGB(128, 0, 0),
         Color::RGB(0, 128, 0),
@@ -142,21 +143,22 @@ fn main() {
         Color::RGB(128, 128, 0),
     ];
 
-        // Creates a vector of threads, where each thread calculates the git statistics for a given author.
-        // The function returns a vector of handles to the threads.
-        let handles: Vec<_> = authors
-            .iter()
-            .enumerate()
-            .map(|(index, author)| {
-                let color = colors[index % colors.len()];
-                let author_clone = author.clone();
+    // Creates a vector of threads, where each thread calculates the git statistics for a given author.
+    // The function returns a vector of handles to the threads.
+    let handles: Vec<_> = authors
+        .iter()
+        .enumerate()
+        .map(|(index, author)| {
+            let color = colors[index % colors.len()];
+            let author_clone = author.clone();
 
-                thread::spawn(move || {
-                    let (author_name, lines_inserted, files_changed): (String, i32, i32) = get_git_stats(&author_clone);
-                    (author_name.clone(), (lines_inserted as f32, color))
-                })
+            thread::spawn(move || {
+                let (author_name, lines_inserted, files_changed): (String, i32, i32) =
+                    get_git_stats(&author_clone);
+                (author_name.clone(), (lines_inserted as f32, color))
             })
-            .collect();
+        })
+        .collect();
 
     // Iterate over the handles and insert the author name and stats into the author_stats HashMap.
     for handle in handles {
@@ -167,26 +169,23 @@ fn main() {
 
     loading.end();
 
-
     // Maps the author statistics to a vector of `Data` structs.
-    // 
+    //
     // # Arguments
-    // 
+    //
     // * `author_stats` - A reference to a `HashMap` containing the author statistics.
-    // 
+    //
     // # Returns
-    // 
+    //
     // A vector of `Data` structs, where each struct contains the author's name, number of lines inserted,
     // color and fill character.
     let data: Vec<Data> = author_stats
         .iter()
-        .map(|(author, (lines_inserted, color))| {
-            Data {
-                label: author.to_string(),
-                value: *lines_inserted,
-                color: Some((*color).into()),
-                fill: '•',
-            }
+        .map(|(author, (lines_inserted, color))| Data {
+            label: author.to_string(),
+            value: *lines_inserted,
+            color: Some((*color).into()),
+            fill: '•',
         })
         .collect();
 
